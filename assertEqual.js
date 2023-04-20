2559f3468b280c61bc8b4c6ff1e7973c02e238cb
NFerracuti <npferracuti@gmail.com>
2023-04-20 16:32:15 -0400

    lotide 1 assert

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+//test code
+assertEqual("Bingo Bongo", "Hello");
+assertEqual(1, 1);
+assertEqual('nick', 'nick');
+assertEqual(1, 4);
