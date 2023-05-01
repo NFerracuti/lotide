3febdf55910aa3f8a6abd6fb77a7348139300498
NFerracuti <npferracuti@gmail.com>
2023-05-01 14:57:33 -0400

    added takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+const eqArrays = function(actual, expected) {
+
+  if (actual.length !== expected.length) {
+
+    return false;
+  }
+    for (let i = 0; i < actual.length; i++) {
+
+      if (expected[i] !== actual[i]) {
+
+        return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const takeUntil = function(arr, callback) {
+  result = [];
+  for (let i = 0; i <= arr.length; i++) {
+    if (!callback(arr[i])) {
+      result.push(arr[i]);
+    } else {
+      break;
+    }
+  } 
+  return result;
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
