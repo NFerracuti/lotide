3c4b8c46639f27cacb2decfd83091af268d6fe14
NFerracuti <npferracuti@gmail.com>
2023-05-11 18:16:49 -0400

    refactoring with mocha

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 const tail = function(x) {
 
   let output = [];
@@ -16,5 +7,6 @@ const tail = function(x) {
   }
   return output;
 };
+
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,33 @@
 const assert = require('chai').assert;
-onst tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
-
+const tail = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-//test to make sure the original array is not being modified
 const f = ["x", "k", "t"];
+
 tail(f);
-assertEqual(f.length, 3);
+describe("#tail", () => {
+  it("returns 3 for f.length", () => {
+    assert.strictEqual((f.length), 3);
+  });
 
-const sing = tail([3]);
-console.log(sing);
+  it("returns 2 for result.length", () => {
+    assert.strictEqual((result.length), 2);
+  });
 
-const emp = tail([]);
-console.log(emp);
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  });
 
+  it("returns 'Labs' for result[1]", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+
+  it("returns [ 2, 3, 4, 5 ] for [1, 2, 3, 4, 5] ", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [ 2, 3, 4, 5 ]);
+  });
+
+  it("returns [ 'how', 'are', 'you' ] for [ 'hello', 'how', 'are', 'you' ] ", () => {
+    assert.deepEqual(tail([ 'hello', 'how', 'are', 'you' ]), [ 'how', 'are', 'you' ]);
+  });
+});
